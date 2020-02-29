d40089a4ee204508e31d1f1c33752141f5335c07
Mehmet Emrah Tunçel <timemrah@gmail.com>
2020-02-29 23:28:10 +0300

    general improved, namings

--- addRemoveUrlQuery.js
+++ addRemoveUrlQuery.js
@@ -1,27 +1,22 @@
-function addRemoveUrlQuery(addParam = {}, removeParam = [], startQueryChar = '?') {
+function addRemoveUrlQuery(addParams, removeParams, startQueryChar = '?'){
+
+    addParams    = addParams || {};
+    removeParams = removeParams || [];
 
     const urlParams = new URLSearchParams(window.location.search);
 
-    addParam = addParam || {}
-
-    // Add param
-    for (const i in addParam) {
-        if (urlParams.has(i)) {
-            urlParams.set(i, addParam[i]);
-        }
-        else {
-            urlParams.append(i, addParam[i]);
-        }
+    //Add param
+    for(const i in addParams){
+        if(urlParams.has(i)){ urlParams.set(i, addParams[i]); }
+        else                { urlParams.append(i, addParams[i]); }
     }
 
-    removeParam = removeParam || []
-
-    // Remove param
-    for (const i of removeParam) {
+    //Remove param
+    for(const i of removeParams){
         urlParams.delete(i);
     }
 
-    if (urlParams.toString()) {
+    if(urlParams.toString()){
         return startQueryChar + urlParams.toString();
     }
 
